83c6c0c2e20bb8264185f5539cafdbe30ce2aaae
Simon Heiss <simonheiss87@gmail.com>
2018-01-02 23:04:06 +0100

    added new shortcut

--- src/ExtractStyledComponentHandler.java
+++ src/ExtractStyledComponentHandler.java
@@ -1,10 +1,11 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
 
 public class ExtractStyledComponentHandler extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+        Object data = e.getData(LangDataKeys.PSI_FILE);
     }
 }
