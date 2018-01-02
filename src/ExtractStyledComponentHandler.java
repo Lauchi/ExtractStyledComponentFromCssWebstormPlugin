c1a0c7c34e94db02a25a4ad624e0b5deacb5841c
Simon Heiss <simonheiss87@gmail.com>
2018-01-03 00:47:54 +0100

    find element at carret

--- src/ExtractStyledComponentHandler.java
+++ src/ExtractStyledComponentHandler.java
@@ -1,26 +1,25 @@
-import com.intellij.lang.ASTNode;
-import com.intellij.lang.FileASTNode;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.tree.TokenSet;
 
 public class ExtractStyledComponentHandler extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
         final PsiFile openedFilePsi = e.getData(LangDataKeys.PSI_FILE);
-        FileASTNode node = openedFilePsi.getNode();
-        TokenSet tokenSet = TokenSet.ANY;
-        ASTNode[] children = node.getChildren(tokenSet);
-        for (final ASTNode child : children) {
-            new WriteCommandAction.Simple(e.getProject(), openedFilePsi.getContainingFile()) {
-                public void run() {
-                    openedFilePsi.add(child.getPsi());
-                }
-            }.execute();
-        }
+        final Editor editor = e.getData(CommonDataKeys.EDITOR);
+
+        final PsiElement elementAt = openedFilePsi.findElementAt(editor.getCaretModel().getOffset());
+
+        new WriteCommandAction.Simple(e.getProject(), openedFilePsi.getContainingFile()) {
+            public void run() {
+                openedFilePsi.add(elementAt);
+            }
+        }.execute();
     }
 }
