b6a74bec2ea1fd8c10f745e57e071568f4d4dc5f
Simon Heiss <simonheiss87@gmail.com>
2018-01-02 20:42:51 +0100

    renaming

--- src/ExtractStyledComponentHandler.java
+++ src/ExtractStyledComponentHandler.java
@@ -4,15 +4,10 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 public class ExtractStyledComponentHandler extends AnAction {
 
     public ExtractStyledComponentHandler() {
-        super("Create _Publish _Script _Handler");
+        super("ExtractStyledComponentHandler");
     }
 
     public void actionPerformed(AnActionEvent event) {
 
-    }
-
-    @Override
-    public void update(AnActionEvent event) {
-       //
     }
 }
