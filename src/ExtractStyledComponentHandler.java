356e5ed157871607674ac920cc45c84391dcc5b4
Simon Heiss <simonheiss87@gmail.com>
2018-01-03 00:22:00 +0100

    figured out how to add ne childs to the syntax tree, just duplicating the file, tough

--- src/ExtractStyledComponentHandler.java
+++ src/ExtractStyledComponentHandler.java
@@ -1,11 +1,26 @@
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.FileASTNode;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.TokenSet;
 
 public class ExtractStyledComponentHandler extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        Object data = e.getData(LangDataKeys.PSI_FILE);
+        final PsiFile openedFilePsi = e.getData(LangDataKeys.PSI_FILE);
+        FileASTNode node = openedFilePsi.getNode();
+        TokenSet tokenSet = TokenSet.ANY;
+        ASTNode[] children = node.getChildren(tokenSet);
+        for (final ASTNode child : children) {
+            new WriteCommandAction.Simple(e.getProject(), openedFilePsi.getContainingFile()) {
+                public void run() {
+                    openedFilePsi.add(child.getPsi());
+                }
+            }.execute();
+        }
     }
 }
