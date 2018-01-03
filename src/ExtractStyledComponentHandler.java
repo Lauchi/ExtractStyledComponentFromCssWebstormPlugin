fb9a8bf499790e07206c93ebb0d253eb93113951
Simon Heiss <simonheiss87@gmail.com>
2018-01-03 19:57:39 +0100

    some randproblem "fixed"

--- src/ExtractStyledComponentHandler.java
+++ src/ExtractStyledComponentHandler.java
@@ -15,8 +15,10 @@ public class ExtractStyledComponentHandler extends AnAction {
         final PsiFile openedFilePsi = e.getData(LangDataKeys.PSI_FILE);
         final Editor editor = e.getData(CommonDataKeys.EDITOR);
 
-        PsiElement selectionStart = openedFilePsi.findElementAt(editor.getSelectionModel().getSelectionStart());
-        PsiElement selectionEnd = openedFilePsi.findElementAt(editor.getSelectionModel().getSelectionEnd());
+        int selectionStart1 = editor.getSelectionModel().getSelectionStart() + 1;
+        PsiElement selectionStart = openedFilePsi.findElementAt(selectionStart1);
+        int selectionEnd1 = editor.getSelectionModel().getSelectionEnd() - 1;
+        PsiElement selectionEnd = openedFilePsi.findElementAt(selectionEnd1);
 
         final PsiElement commonParent = PsiTreeUtil.findCommonParent(selectionStart, selectionEnd);
 
